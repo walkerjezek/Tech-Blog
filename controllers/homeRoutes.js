9859bd3de29006e68a690278a661c98c6b7647fa
walkerj <wpjezek@gmail.com>
2022-05-29 13:41:26 -0600

    yay something is showing up now

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,7 +5,7 @@ const withAuth = require('../utils/auth');
 router.get('/', async (req, res) => {
   try {
     // Get all posts and JOIN with user data
-    const postData = await Post.findAll({
+    const postData = await Posts.findAll({
       include: [
         {
           model: User,
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
     });
 
     // Serialize data so the template can read it
-    const posts = postData.map((post) => Post.get({ plain: true }));
+    const posts = postData.map((post) => Posts.get({ plain: true }));
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
